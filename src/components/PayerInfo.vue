a5d4595d7b466b67b118c5103387f23a7437682b
Iago Puzer <iagopuzer@hotmail.com>
2022-09-01 01:14:56 -0300

    Inserção dos dados dinamicos e ajustes nos componentes

---
+++ src/components/PayerInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="payer-details mt-4 ml-2" v-for="client in clients" :key="client">
+      <ul>
+        <li>Nome: {{client.name}}</li>
+        <li>Documento:</li>
+        <li>Telefone:</li>
+        <li>Email:</li>
+        <li>data de criação:</li>
+        <li>id:</li>
+      </ul>  
+    </div>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+export default {
+    name: "payerinfo",
+
+    computed: mapState([
+        'clients'
+    ]),
+}
+</script>
+
+<style scoped>
+
+.payer-details li {
+  @apply px-6 py-3 text-left text-xs font-medium text-gray-600 uppercase tracking-wider;
+}
+
+</style>
